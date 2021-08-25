6e12ea5a21f071bb25b2d94a2b619e2a138b683d
Dhruv Gupta <dhruv4@gmail.com>
2021-08-25 12:58:34 -0400

    add header image

--- src/components/sections/header.js
+++ src/components/sections/header.js
@@ -8,7 +8,7 @@ import { Container } from "../global"
 const Header = () => {
   const data = useStaticQuery(graphql`
     query {
-      file(sourceInstanceName: { eq: "product" }, name: { eq: "green-skew" }) {
+      file(sourceInstanceName: { eq: "product" }, name: { eq: "axle-header" }) {
         childImageSharp {
           fluid(maxWidth: 1000) {
             ...GatsbyImageSharpFluid_tracedSVG
