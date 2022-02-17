fe90e80f2677fe110c3e6c60070b86ed7422b62f
Dhruv Gupta <dhruv4@gmail.com>
2022-02-16 20:37:21 -0500

    Updates

---
+++ react-syntax-highlighter_languages_refractor_brainfuck-34144442c3b3cb6e0b23.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[29],{czuR:function(a,e,n){"use strict";function t(a){a.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}a.exports=t,t.displayName="brainfuck",t.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck-34144442c3b3cb6e0b23.js.map
