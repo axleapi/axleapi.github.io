aec2f73c8b51593f46de03d5aa39a89594746f65
Dhruv Gupta <dhruv4@gmail.com>
2022-06-29 12:18:19 -0400

    add path to careers

---
+++ src/pages/careers.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+import Layout from "../components/common/layout/layout"
+import SEO from "../components/common/layout/seo"
+import Navigation from "../components/common/navigation/navigation"
+
+import Header from "../components/sections/header"
+import Solutions from "../components/sections/solutions"
+import Footer from "../components/sections/footer"
+import GetStarted from "../components/sections/getstarted"
+import Product from "../components/sections/product"
+import Developers from "../components/sections/developers"
+import { navigate } from "gatsby"
+
+const CareersPage = () => {
+  window.location.href = "https://axleapi.notion.site/Careers-at-Axle-df4daa9afb064b20b94acc2c4c4417bc"
+}
+
+export default CareersPage
